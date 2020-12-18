ec2df9f70f0bab75021ebef4e50a0fada6bdc1c7
AnthonySantillán <fairy.jum568@gmail.com>
2020-12-17 23:06:48 -0500

    Escepciones

---
+++ Excepciones/Excepciones/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Excepciones
+{
+    class Program
+    {
+
+        public static List<String> MiLista;
+        static void Main(string[] args)
+        {
+           //MiLista = new List<string>();
+            //acciona realizar
+            try
+            {
+                MiLista.Add("Objeto 1");
+                Console.WriteLine("Objeta agregado");
+
+            }
+            //accion en caso de que no funcione
+            catch (Exception) 
+            {
+
+                Console.WriteLine("Algo sale mal");
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
